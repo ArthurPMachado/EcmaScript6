bd4fa9270f23d4038ae62d7e1f23365decee2e36
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-09-16 16:44:39 -0300

    Feat: create render method to display repositories

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
     this.repositories = [];
     
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers();
   }
@@ -21,7 +22,34 @@ class App {
       html_url: 'http://github.com/rocketseat/comunidade'
     });
 
-    console.log(this.repositories);
+    this.render();
+  }
+  
+  render() {
+    this.listEl.innerHTML = '';
+
+    this.repositories.forEach(item => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', item.avatar_url);
+      
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(item.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(item.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
